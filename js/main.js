93148659e11d6152884415ed47d120bebb5f13a5
Miguel Condori <miguel.angel.condori@gmail.com>
2022-09-18 00:00:04 -0300

    week1

--- js/main.js
+++ js/main.js
@@ -16,20 +16,19 @@ links.forEach(listlinks);
     function listlinks() {
   
       // Create elements to add to the document
+      let ol = document.querySelector('weeks')
       let li = document.createElement('li');
       let a = document.createElement("a");       
       
       var linkText = document.createTextNode(`${links.label}`);
-      li.appendChild(linkText);
-      a.textContent = `${links.label}`;
+      a.appendChild(linkText);
       a.href = links.url;
      
       // Add/append the section(card) with the element
       
       li.appendChild(a);
       
-      // Add/append the existing HTML div with the cards class with the section(card)
-      document.querySelector('ol.weeks').appendChild(li);
+      ol.appendChild(li);
 
         
       }
