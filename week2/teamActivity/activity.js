8d3badfd0e4485fe99f00d77dad7e08bb86f26cf
Miguel Condori <miguel.angel.condori@gmail.com>
2022-09-22 21:24:33 -0300

    Team activity

--- week2/teamActivity/activity.js
+++ week2/teamActivity/activity.js
@@ -4,7 +4,7 @@ function copyText()
 
         var inputText = document.getElementById("inputText").value;
        
-        let div = document.querySelector("#one") 
+        let div = document.querySelector("#one");
         let p = document.createElement('p');
 
         p.innerHTML = inputText;
@@ -17,12 +17,12 @@ function summing(inputNumber)
     {
 
         let total = 0;
-        iNumber = Number(inputNumber)
+        iNumber = Number(inputNumber);
         for(iNumber; iNumber > 0; iNumber--) {
             total += iNumber;
         }
 
-        let div = document.querySelector("#two") 
+        let div = document.querySelector("#two");
         let p = document.createElement('p');
 
         p.innerHTML = total;
@@ -30,17 +30,17 @@ function summing(inputNumber)
         div.appendChild(p);
     
     }
-
+// function declaration
 function adding() 
     {
         var Number1 = document.getElementById("inputNumber1").value;
         var Number2 = document.getElementById("inputNumber2").value;
         let total = 0;
-        Number1 = Number(Number1)
-        Number2 = Number(Number2)
-        total = Number1 + Number2
+        Number1 = Number(Number1);
+        Number2 = Number(Number2);
+        total = Number1 + Number2;
 
-        let div = document.querySelector("#three") 
+        let div = document.querySelector("#three");
         let p = document.createElement('p');
 
         p.innerHTML = total;
@@ -48,3 +48,56 @@ function adding()
         div.appendChild(p);
     
     }
+
+//Arrow function
+const subtract = () =>
+    {
+        var Number1 = document.getElementById("Num1").value;
+        var Number2 = document.getElementById("Num2").value;
+        let total = 0;
+        Number1 = Number(Number1);
+        Number2 = Number(Number2);
+        total = Number1 - Number2;
+
+        let div = document.querySelector("#oneB");
+        let p = document.createElement('p');
+
+        p.innerHTML = total;
+
+        div.appendChild(p);
+    }
+//function expression
+    const multiply = function () {
+    
+        var Number1 = document.getElementById("Num1").value;
+        var Number2 = document.getElementById("Num2").value;
+        let total = 0;
+        Number1 = Number(Number1);
+        Number2 = Number(Number2);
+        total = Number1 * Number2;
+
+        let div = document.querySelector("#oneB");
+        let p = document.createElement('p');
+
+        p.innerHTML = total;
+
+        div.appendChild(p);
+    };
+// function declaration
+function division() 
+{
+    var Number1 = document.getElementById("Num1").value;
+    var Number2 = document.getElementById("Num2").value;
+    let total = 0;
+    Number1 = Number(Number1);
+    Number2 = Number(Number2);
+    total = Number1 / Number2;
+
+    let div = document.querySelector("#oneB");
+    let p = document.createElement('p');
+
+    p.innerHTML = total;
+    
+    div.appendChild(p);
+
+}
